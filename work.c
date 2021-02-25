c2c4b39ee5a393e2f34d060a8f5aba940e4fef5d
Turan-c <13275817339@163.com>
2021-02-25 14:54:57 +0800

    modified:   work.c

--- work.c
+++ work.c
@@ -2,24 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-	int i;
-	int length = 0;
-	scanf("%d", &length);
-	int array[length];
-	// printf("Please enter the large of string\n");
-	for(i = 0; i < length; i ++){
-		array[i] = 0;
+	int ch;
+	while((ch = getchar()) != EOF){
+		putchar(ch);
 	}
-	// printf("Please enter the number of string\n");
-	for(i = 0; i < length; i ++){
-		scanf("%d", &array[i]);
-	}
-
-
-	for (i = 0; i < length; i ++)
-	{
-		printf("%d\n", array[i]);
-	}
-	
+		
 	return 0;
 }
